458dba3c71d74dfa975e1d14d8a456ff90b5f79b
Bartolomé Serapio <bartolome.serapio@trivago.com>
2017-07-10 09:55:21 +0200

    Create endpoints, create service to call to the API

---
+++ src/services/client.js
@@ -0,0 +1,42 @@
+
+import request from 'shared/request'
+import apiEndPoints from 'shared/endpoints'
+
+function get(id) {
+
+    let url = apiEndPoints.clientDetail.replace(':id',id);
+    return request({
+        url:    url,
+        method: 'GET'
+    });
+}
+function put(id,data) {
+
+    let url = apiEndPoints.clientDetail.replace(':id',id);
+    return request({
+        url:    url,
+        method: 'POST',
+        data:data
+    });
+}
+function list(limit=10,page=1) {
+
+    return request({
+        url: apiEndPoints.userList,
+        method:'GET',
+        data : {
+            limit: limit,
+            page: page
+        }
+    })
+}
+function create(data) {
+    return request({
+        url: apiEndPoints.userList,
+        method: 'POST',
+        data:  data
+    });
+}
+const clientService = { get, create,list,put };
+
+export default clientService;
---
+++ src/services/shared/endpoints.js
@@ -0,0 +1,12 @@
+const apiEndPoints = {
+    baseUrl:'/',
+    userList:     'api/user',
+    userDetail:  'api/user/:id',
+    userClientList:  'api/user/:id/client',
+    userClientDetail:'api/user/:id/client/:idMeta',
+    clientList:'api/client',
+    clientDetail:'api/client/:id',
+
+};
+
+export default apiEndPoints;
